7789cd89d4e3f6efb593bed5cc926cbcff6a9155
Shrey0207 <shreytolasaria4297@gmail.com>
2024-06-17 23:47:44 +0530

    added calorie calculator

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 import streamlit as st
 import joblib
 import pandas as pd
+import google.generativeai as genai
+import google.ai.generativelanguage as glm
+from PIL import Image
 
-# Set the app to wide mode
-st.set_page_config(layout="wide")
+# Set the app configuration
+st.set_page_config(page_title="FitCheck", page_icon="🔥", layout="wide", initial_sidebar_state='expanded')
 
 # Load the trained model
 model = joblib.load('decision_tree_model.pkl')
@@ -54,92 +57,153 @@ class Person:
         maintain_calories = self.calculate_bmr() * weight_factor
         return maintain_calories
 
-# Define the user interface
-st.title("Obesity and Waist Size Risk Score Prediction")
+# Configure Google Generative AI
+API_KEY = "AIzaSyAo9yfpvJACfzgxPyX3cj3FkSoV4wUy3nY"
+genai.configure(api_key=API_KEY)
 
-# Input fields
-age = st.number_input("Age", min_value=1, max_value=100, value=25)
-waist = st.number_input("Waist (cm)", min_value=30, max_value=200, value=80)
-weight = st.number_input("Weight (kg)", min_value=10, max_value=300, value=70)
-height = st.number_input("Height (cm)", min_value=50, max_value=250, value=170)
+# Navigation Bar
+page = st.sidebar.selectbox("Navigation", ["Home", "Obesity Prediction", "Calories Calculator", "About Us"])
 
-# Zone selection
-zone = st.selectbox("Zone", options=["N", "S", "E", "W", "NW", "NE", "SW", "SE"])
+# Home Page
+if page == "Home":
+    st.title("Welcome to FitCheck")
+    st.write("Home page content goes here...")
 
-# Gender selection
-gender = st.selectbox("Gender", options=["Male", "Female"])
+# Obesity Prediction Page
+elif page == "Obesity Prediction":
+    st.title("Obesity and Waist Size Risk Score Prediction")
 
-# Physical activity inputs
-moderate_activity = st.slider("Moderate Physical Activity (level)", min_value=0, max_value=6, value=1)
-vigorous_activity = st.slider("Vigorous Physical Activity (level)", min_value=0, max_value=6, value=1)
-daily_activity = st.slider("Daily Physical Activity (level)", min_value=0, max_value=6, value=1)
+    # Input fields
+    age = st.number_input("Age", min_value=1, max_value=100, value=25)
+    waist = st.number_input("Waist (cm)", min_value=30, max_value=200, value=80)
+    weight = st.number_input("Weight (kg)", min_value=10, max_value=300, value=70)
+    height = st.number_input("Height (cm)", min_value=50, max_value=250, value=170)
 
-# Predict button
-if st.button("Predict"):
-    # Calculate BMI
-    person = Person(age, height, weight, gender, 'Moderate exercise (3-5 days/wk)')  # For calorie calculations
-    height_m = height / 100
-    bmi_calculated = weight / (height_m ** 2)
+    # Zone selection
+    zone = st.selectbox("Zone", options=["N", "S", "E", "W", "NW", "NE", "SW", "SE"])
 
-    # Create a DataFrame for the input data
-    input_data = pd.DataFrame({
-        'Age': [age],
-        'Waist': [waist],
-        'Weight': [weight],
-        'Height': [height],
-        'Zone': [zone],
-        'Gender': [gender],
-        'Moderatephysicalactivity': [moderate_activity],
-        'Vigorousphysicalactivity': [vigorous_activity],
-        'Dailyphysicalactivity': [daily_activity],
-        'Bmi_calculated': [bmi_calculated]
-    })
+    # Gender selection
+    gender = st.selectbox("Gender", options=["Male", "Female"])
 
-    # Perform the prediction
-    prediction = model.predict(input_data)
-    risk_score = prediction[0]
+    # Physical activity inputs
+    moderate_activity = st.slider("Moderate Physical Activity (level)", min_value=0, max_value=6, value=1)
+    vigorous_activity = st.slider("Vigorous Physical Activity (level)", min_value=0, max_value=6, value=1)
+    daily_activity = st.slider("Daily Physical Activity (level)", min_value=0, max_value=6, value=1)
 
-    # Determine risk category and color
-    if risk_score == 0:
-        risk_category = 'Insufficient weight'
-        risk_color = 'Blue'
-    elif risk_score == 1:
-        risk_category = 'Normal weight'
-        risk_color = 'Green'
-    elif risk_score == 2:
-        risk_category = 'Overweight'
-        risk_color = 'Yellow'
-    elif risk_score == 3:
-        risk_category = 'Obesity Type 1'
-        risk_color = 'Orange'
-    elif risk_score == 4:
-        risk_category = 'Obesity Type 2'
-        risk_color = 'Red'
-    else:
-        risk_category = 'Obesity Type 3'
-        risk_color = 'Dark Red'
+    # Predict button
+    if st.button("Predict"):
+        # Calculate BMI
+        person = Person(age, height, weight, gender, 'Moderate exercise (3-5 days/wk)')  # For calorie calculations
+        height_m = height / 100
+        bmi_calculated = weight / (height_m ** 2)
 
-    # Display the prediction result
-    st.write(f"The predicted Obesity and Waist Size Risk Score is: {risk_score}")
-    new_title = f'<p style="font-family:sans-serif; color:{risk_color}; font-size: 25px;">{risk_category}</p>'
-    st.markdown(new_title, unsafe_allow_html=True)
+        # Create a DataFrame for the input data
+        input_data = pd.DataFrame({
+            'Age': [age],
+            'Waist': [waist],
+            'Weight': [weight],
+            'Height': [height],
+            'Zone': [zone],
+            'Gender': [gender],
+            'Moderatephysicalactivity': [moderate_activity],
+            'Vigorousphysicalactivity': [vigorous_activity],
+            'Dailyphysicalactivity': [daily_activity],
+            'Bmi_calculated': [bmi_calculated]
+        })
 
-    # Display BMI result
-    st.header('BMI Calculator')
-    bmi_string, bmi_category, bmi_color = person.display_result()
-    st.metric(label="Body Mass Index (BMI)", value=bmi_string)
-    new_title = f'<p style="font-family:sans-serif; color:{bmi_color}; font-size: 25px;">{bmi_category}</p>'
-    st.markdown(new_title, unsafe_allow_html=True)
-    st.markdown("Healthy BMI range: 18.5 kg/m² - 25 kg/m².")
+        # Perform the prediction
+        prediction = model.predict(input_data)
+        risk_score = prediction[0]
 
-    # Display calories calculator result
-    st.header('Calories Calculator')
-    maintain_calories = person.calories_calculator()
-    st.write('The results show a number of daily calorie estimates that can be used as a guideline for how many calories to consume each day to maintain, lose, or gain weight at a chosen rate.')
-    plans = ["Maintain weight", "Mild weight loss", "Weight loss", "Extreme weight loss"]
-    weights = [1, 0.9, 0.8, 0.6]
-    losses = ['-0 kg/week', '-0.25 kg/week', '-0.5 kg/week', '-1 kg/week']
-    for plan, weight_factor, loss, col in zip(plans, weights, losses, st.columns(4)):
-        with col:
-            st.metric(label=plan, value=f'{round(maintain_calories * weight_factor)} Calories/day', delta=loss, delta_color="inverse")
+        # Determine risk category and color
+        if risk_score == 0:
+            risk_category = 'Insufficient weight'
+            risk_color = 'Blue'
+        elif risk_score == 1:
+            risk_category = 'Normal weight'
+            risk_color = 'Green'
+        elif risk_score == 2:
+            risk_category = 'Overweight'
+            risk_color = 'Yellow'
+        elif risk_score == 3:
+            risk_category = 'Obesity Type 1'
+            risk_color = 'Orange'
+        elif risk_score == 4:
+            risk_category = 'Obesity Type 2'
+            risk_color = 'Blue'
+        else:
+            risk_category = 'Obesity Type 3'
+            risk_color = 'Red'
 
+        # Display the prediction result
+        st.write(f"The predicted Obesity and Waist Size Risk Score is: {risk_score}")
+        new_title = f'<p style="font-family:sans-serif; color:{risk_color}; font-size: 25px;">{risk_category}</p>'
+        st.markdown(new_title, unsafe_allow_html=True)
+
+        # Display BMI result
+        st.header('BMI Calculator')
+        bmi_string, bmi_category, bmi_color = person.display_result()
+        st.metric(label="Body Mass Index (BMI)", value=bmi_string)
+        new_title = f'<p style="font-family:sans-serif; color:{bmi_color}; font-size: 25px;">{bmi_category}</p>'
+        st.markdown(new_title, unsafe_allow_html=True)
+        st.markdown("Healthy BMI range: 18.5 kg/m² - 25 kg/m².")
+
+        # Display calories calculator result
+        st.header('Calories Calculator')
+        maintain_calories = person.calories_calculator()
+        st.write('The results show a number of daily calorie estimates that can be used as a guideline for how many calories to consume each day to maintain, lose, or gain weight at a chosen rate.')
+        plans = ["Maintain weight", "Mild weight loss", "Weight loss", "Extreme weight loss"]
+        weights = [1, 0.9, 0.8, 0.6]
+        losses = ['-0 kg/week', '-0.25 kg/week', '-0.5 kg/week', '-1 kg/week']
+        for plan, weight_factor, loss, col in zip(plans, weights, losses, st.columns(4)):
+            with col:
+                st.metric(label=plan, value=f'{round(maintain_calories * weight_factor)} Calories/day', delta=loss, delta_color="inverse")
+
+# Calories Calculator Page
+elif page == "Calories Calculator":
+    st.header("Calories Calculator")
+    uploaded_file = st.file_uploader("Upload an Image file", accept_multiple_files=False, type=['jpg', 'png', 'jfif'])
+    
+    if uploaded_file is not None:
+        image = Image.open(uploaded_file)
+        st.image(image, caption='Uploaded Image', use_column_width=True)
+        bytes_data = uploaded_file.getvalue()
+        
+        prompt = """You are a nutritionist and given an uploaded image of a meal, calculate the calories for each individual food item present 
+                 and provide the results in separate lines. Additionally, include a line for the total calorie count 
+                 of the entire meal. Please specify any key factors affecting the calculation, such as portion size 
+                 or specific ingredients visible in the image. Ensure the calorie estimates are as accurate as 
+                 possible based on the visual information provided.
+                 Results should should be in the format 
+                 1. Item1- number of calories
+                    ----
+                    ----
+                 and so on"""
+
+        generate = st.button("Calculate")
+        if generate:
+            try:
+                model = genai.GenerativeModel('gemini-pro-vision')
+                response = model.generate_content(
+                    glm.Content(
+                        parts=[
+                            glm.Part(text=prompt),
+                            glm.Part(
+                                inline_data=glm.Blob(
+                                    mime_type='image/jpeg',
+                                    data=bytes_data
+                                )
+                            ),
+                        ],
+                    ),
+                    stream=True
+                )
+
+                response.resolve()
+                st.write(response.text)
+            except:
+                st.write("Error! Check the prompt or uploaded image")
+else:
+    # About Us Page
+    st.title("About Us")
+    st.write("About us content goes here...")
