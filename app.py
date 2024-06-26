d346aa7057dab660d9c37313213b9ad205ec6a14
Shrey0207 <shreytolasaria4297@gmail.com>
2024-06-26 16:31:49 +0530

    upgraded the app to latest gemini model

--- app.py
+++ app.py
@@ -158,48 +158,73 @@ elif page == "Obesity Prediction":
 
 # Calories Calculator Page
 elif page == "Calories Calculator":
-    st.header("Calories Calculator")
-    uploaded_file = st.file_uploader("Upload an Image file", accept_multiple_files=False, type=['jpg', 'png', 'jfif'])
+    st.title("Gemini NutriAI 🍽️")
+    # Sidebar guide
+    st.sidebar.markdown("""
+    ### Guide
+    1. Enter your Gemini API key in the provided input field.
+    2. The default prompt is already integrated into the application.
+    3. Simply upload a photo of food to receive calorie information.
+    4. For additional details or a custom prompt, utilize the "Provide Prompt" input to access extra information via the Gemini Vision Pro model.
+    """)
+    # Configure Google Gemini Pro Vision API with the API key from the input field
+    api_key = st.sidebar.text_input("Enter your Google API Key:", key="api_key")
+    if api_key:
+         genai.configure(api_key=api_key)
+    else:
+        st.warning("Please enter your Google API Key.")
+
+    # Guide for obtaining Google API Key if not available
+    st.sidebar.subheader("Don't have a Google API Key?")
+    st.sidebar.write("Visit [Google Makersuite](https://makersuite.google.com/app/apikey) and log in with your Google account. Then click on 'Create API Key'.")
+    ## Function to load Google Gemini Pro Vision API And get response
+    def get_gemini_response(input, image, prompt):
+        model = genai.GenerativeModel('gemini-pro-vision')
+        response = model.generate_content([input, image[0], prompt])
+        return response.text
+    def input_image_setup(uploaded_file):
+        # Check if a file has been uploaded
+        if uploaded_file is not None:
+            # Read the file into bytes
+            bytes_data = uploaded_file.getvalue()
+
+            image_parts = [
+            {
+                "mime_type": uploaded_file.type,  # Get the mime type of the uploaded file
+                "data": bytes_data
+            }
+        ]
+            return image_parts
+        else:
+            raise FileNotFoundError("No file uploaded")
+        
     
+    uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+    image = ""
     if uploaded_file is not None:
         image = Image.open(uploaded_file)
-        st.image(image, caption='Uploaded Image', use_column_width=True)
-        bytes_data = uploaded_file.getvalue()
-        
-        prompt = """You are a nutritionist and given an uploaded image of a meal, calculate the calories for each individual food item present 
-                 and provide the results in separate lines. Additionally, include a line for the total calorie count 
-                 of the entire meal. Please specify any key factors affecting the calculation, such as portion size 
-                 or specific ingredients visible in the image. Ensure the calorie estimates are as accurate as 
-                 possible based on the visual information provided.
-                 Results should should be in the format 
-                 1. Item1- number of calories
-                    ----
-                    ----
-                 and so on"""
+        st.image(image, caption="Uploaded Image.", use_column_width=True)
 
-        generate = st.button("Calculate")
-        if generate:
-            try:
-                model = genai.GenerativeModel('gemini-pro-vision')
-                response = model.generate_content(
-                    glm.Content(
-                        parts=[
-                            glm.Part(text=prompt),
-                            glm.Part(
-                                inline_data=glm.Blob(
-                                    mime_type='image/jpeg',
-                                    data=bytes_data
-                                )
-                            ),
-                        ],
-                    ),
-                    stream=True
-                )
+    submit = st.button("Tell me the total calories")
 
-                response.resolve()
-                st.write(response.text)
-            except:
-                st.write("Error! Check the prompt or uploaded image")
+    input_prompt=""" You are a nutritionist and given an uploaded image of a meal, calculate the calories for each individual food item present 
+             and provide the results in separate lines. Additionally, include a line for the total calorie count 
+             of the entire meal. Please specify any key factors affecting the calculation, such as portion size 
+             or specific ingredients visible in the image. Ensure the calorie estimates are as accurate as 
+             possible based on the visual information provided.
+             Results should should be in the format 
+             1. Item1- number of calories
+                ----
+                ----
+             and so on"""
+    
+    ## If submit button is clicked
+    if submit:
+        image_data = input_image_setup(uploaded_file)
+        response = get_gemini_response(input_prompt, image_data, "")
+        st.subheader("The Response is")
+        st.write(response)
+
 else:
     # About Us Page
     st.title("About Us")
